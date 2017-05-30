3cacec7309086097dc0952bdb2e8ced9799ffa53
olgatron <o.dmitricenko@gmail.com>
2017-05-30 22:13:00 +0200

    add game and assets

---
+++ js/game.js
@@ -0,0 +1,36 @@
+var game = new Phaser.Game(800, 640, Phaser.CANVAS, 'game', {
+    preload: preload,
+    create: create,
+    update: update
+})
+var player, key = {},
+    g = {}
+var currentSpeed = 0,
+    gamepadStatus = false
+
+function preload() {
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL
+    game.scale.pageAlignHorizontally = true
+    game.scale.pageAlignVertically = true
+    game.stage.backgroundColor = '#9576AA'
+    game.load.image('boat', 'img/cat.png')
+
+}
+
+function create() {
+    game.add.sprite(0, 0, 'bg')
+    createPlayer()
+}
+
+function update() {
+    game.physics.arcade.velocityFromRotation(player.rotation, currentSpeed, player.body.velocity)
+    gamepadControls()
+}
+
+function createPlayer() {
+
+}
+
+function gamepadControls() {
+
+}
